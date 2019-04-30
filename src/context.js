f731dc66423ca204e4697d2c08ded63172c7352c
joeface71 <joefacenda@yahoo.com>
2019-04-30 08:57:03 -0400

    uploaded images

---
+++ src/context.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
+
+const ProductContext = React.createContext();
+//Provider
+//Consumer
+
+class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+
+  handleData = () => {
+    console.log("hello from detail");
+  };
+
+  addToCart = () => {
+    console.log("hello from add to cart");
+  };
+
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleData: this.handleData,
+          addToCart: this.addToCart
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
